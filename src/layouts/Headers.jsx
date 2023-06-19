0513b4e3ebbb2d1f013b6bb3504486a673827bfe
jaishinhong <jai753198@gmail.com>
2023-06-19 20:01:17 +0700

    header

--- src/layouts/Headers.jsx
+++ src/layouts/Headers.jsx
@@ -1,3 +1,15 @@
 export default function Headers() {
-    return <div >Headers</div>;
+    return (
+        <div className="min-w-full flex justify-between h-[100px] items-center px-10">
+            <h1>Evender</h1>
+            <div className="flex gap-3">
+                <h1>about us</h1>
+                <h1>blog</h1>
+            </div>
+            <div className="flex gap-3">
+                <h1>search icon</h1>
+                <h1>user icon</h1>
+            </div>
+        </div>
+    );
 }
