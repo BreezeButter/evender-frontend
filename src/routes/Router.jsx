d5c9360ed4c376bb3006a5bc3c30a9b84e9a4fe9
TaechitT <t.taechit@hotmail.com>
2023-06-21 19:32:44 +0700

    eventDetailPage

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -8,8 +8,7 @@ import ProfileUser from "../page/ProfileUser";
 // import EventPage from '../page/EventPage';
 // import { Search } from 'react-router-dom';
 import SearchPage from "../page/SearchPage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Container from "../layouts/Container";
+
 // import Landing from '../page/Landing';
 import RegisterPage from "../page/RegisterPage";
 import LoginPage from "../page/LoginPage";
@@ -38,6 +37,10 @@ const router = createBrowserRouter([
                 element: <Landing />,
                 path: "/",
                 element: <RegisterPage />,
+            },
+            {
+                path: "eventdetails/:id",
+                // element: <EventDetails />,
             },
             {
                 path: "/login",
