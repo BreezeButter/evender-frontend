f06a030f73aac69131ffa5c0e265e36ab9feb8d7
bryce-brny <brightbrgh@gmail.com>
2023-06-21 23:45:17 +0700

    done login google API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { ToastContainer } from 'react-toastify';
-import Router from './routes/Router';
+import { ToastContainer } from "react-toastify";
+import Router from "./routes/Router";
 
 function App() {
     return (
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Container from "../layouts/Container";
+// import Container from "../layouts/Container";
 import EditProfile from "../page/EditProfile";
 import Landing from "../page/Landing";
 import ProfileUser from "../page/ProfileUser";
@@ -8,7 +8,7 @@ import ProfileUser from "../page/ProfileUser";
 // import EventPage from '../page/EventPage';
 // import { Search } from 'react-router-dom';
 import SearchPage from "../page/SearchPage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Container from "../layouts/Container";
 // import Landing from '../page/Landing';
 import RegisterPage from "../page/RegisterPage";
