ce3396530d61c313a03f83d12c625e5f06022a5b
jaishinhong <jai753198@gmail.com>
2023-06-24 11:36:03 +0700

    caht

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,55 +1,55 @@
-import { useState, useEffect } from 'react';
-import { io } from 'socket.io-client';
+// import { useState, useEffect } from "react";
+// import { io } from "socket.io-client";
 
-const socket = io.connect('http://localhost:8888');
-export default function Chat() {
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-    const [currentRoom, setCurrentRoom] = useState(''); // Set the initial room
+// const socket = io.connect("http://localhost:8888");
+// export default function Chat() {
+//     const [message, setMessage] = useState("");
+//     const [messages, setMessages] = useState([]);
+//     const [currentRoom, setCurrentRoom] = useState(""); // Set the initial room
 
-    useEffect(() => {
-        socket.auth = { id: 'user01' };
+//     useEffect(() => {
+//         socket.auth = { id: "user01" };
 
-        socket.connect();
-        // Listen for incoming messages
+//         socket.connect();
+//         // Listen for incoming messages
 
-        socket.on('receiveMessage', (message) => {
-            setMessages((prevMessages) => [...prevMessages, message]);
-        });
-        // Clean up the socket connection on unmount
-        return () => {
-            socket.disconnect();
-        };
-    }, []);
-    const joinRoom = (room) => {
-        setCurrentRoom(room);
-        socket.emit('join_room', room);
-    };
+//         socket.on("receiveMessage", (message) => {
+//             setMessages((prevMessages) => [...prevMessages, message]);
+//         });
+//         // Clean up the socket connection on unmount
+//         return () => {
+//             socket.disconnect();
+//         };
+//     }, []);
+//     const joinRoom = (room) => {
+//         setCurrentRoom(room);
+//         socket.emit("join_room", room);
+//     };
 
-    const sendMessage = () => {
-        socket.emit('sendMessage', { room: currentRoom, message });
-        setMessages((prevMessages) => [...prevMessages, message]);
+//     const sendMessage = () => {
+//         socket.emit("sendMessage", { room: currentRoom, message });
+//         setMessages((prevMessages) => [...prevMessages, message]);
 
-        setMessage('');
-    };
+//         setMessage("");
+//     };
 
-    return (
-        <div>
-            <h1>Current Room: {currentRoom}</h1>
-            <button onClick={() => joinRoom('room1')}>Join Room 1</button>
-            <button onClick={() => joinRoom('room2')}>Join Room 2</button>
+//     return (
+//         <div>
+//             <h1>Current Room: {currentRoom}</h1>
+//             <button onClick={() => joinRoom("room1")}>Join Room 1</button>
+//             <button onClick={() => joinRoom("room2")}>Join Room 2</button>
 
-            <ul>
-                {messages.map((msg, index) => (
-                    <li key={index}>{msg}</li>
-                ))}
-            </ul>
-            <input
-                type="text"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-            />
-            <button onClick={sendMessage}>Send</button>
-        </div>
-    );
-}
+//             <ul>
+//                 {messages.map((msg, index) => (
+//                     <li key={index}>{msg}</li>
+//                 ))}
+//             </ul>
+//             <input
+//                 type="text"
+//                 value={message}
+//                 onChange={(e) => setMessage(e.target.value)}
+//             />
+//             <button onClick={sendMessage}>Send</button>
+//         </div>
+//     );
+// }
--- src/page/Chat.jsx
+++ src/page/Chat.jsx
@@ -1,3 +1,117 @@
+import { io } from "socket.io-client";
+import { useState, useEffect } from "react";
+
+const socket = io.connect("http://localhost:8888");
 export default function Chat() {
-    return <div>Chat</div>;
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    const [currentRoom, setCurrentRoom] = useState("");
+    const eventRoomId = "room1";
+    const userId = "user1";
+    useEffect(() => {
+        socket.auth = { id: "user01" };
+
+        socket.connect();
+
+        socket.on("receiveMessage", (message) => {
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    const joinRoom = (room) => {
+        setCurrentRoom(room);
+        socket.emit("joinRoom", room);
+    };
+
+    const sendMessage = () => {
+        socket.emit("sendMessage", { room: currentRoom, message });
+        setMessages((prevMessages) => [...prevMessages, message]);
+
+        setMessage("");
+    };
+
+    return (
+        <div>
+            <h1>Current Room: {currentRoom}</h1>
+            <button onClick={() => joinRoom("room1")}>Join Room 1</button>
+            <button onClick={() => joinRoom("room2")}>Join Room 2</button>
+
+            <ul>
+                {messages.map((msg, index) => (
+                    <li key={index}>{msg}</li>
+                ))}
+            </ul>
+            <input
+                type="text"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <button onClick={sendMessage}>Send</button>
+        </div>
+    );
 }
+
+//#############################################################################
+
+// import { io } from "socket.io-client";
+// import { useState, useEffect } from "react";
+
+// const socket = io.connect("http://localhost:8888");
+// export default function Chat() {
+//     const [message, setMessage] = useState("");
+//     const [messages, setMessages] = useState([]);
+//     const [currentRoom, setCurrentRoom] = useState("");
+//     const eventRoomId = "room1";
+//     const userId = "user1";
+
+//     useEffect(() => {
+//         socket.auth = { id: "user01" };
+
+//         socket.connect();
+
+//         socket.on("receiveMessage", (message) => {
+//             setMessages((prevMessages) => [...prevMessages, message]);
+//         });
+
+//         return () => {
+//             socket.disconnect();
+//         };
+//     }, []);
+
+//     const joinRoom = (room) => {
+//         setCurrentRoom(room);
+//         socket.emit("joinRoom", room);
+//     };
+
+//     const sendMessage = () => {
+//         socket.emit("sendMessage", { room: currentRoom, message });
+//         setMessages((prevMessages) => [...prevMessages, message]);
+
+//         setMessage("");
+//     };
+
+//     return (
+//         <div>
+//             <h1>Current Room: {currentRoom}</h1>
+//             <button onClick={() => joinRoom("room1")}>Join Room 1</button>
+//             <button onClick={() => joinRoom("room2")}>Join Room 2</button>
+
+//             <ul>
+//                 {messages.map((msg, index) => (
+//                     <li key={index}>{msg}</li>
+//                 ))}
+//             </ul>
+//             <input
+//                 type="text"
+//                 value={message}
+//                 onChange={(e) => setMessage(e.target.value)}
+//             />
+//             <button onClick={sendMessage}>Send</button>
+//         </div>
+//     );
+// }
