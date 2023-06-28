c41ae02baaf1bc31911417e3b1c95264022cef63
bryce-brny <brightbrgh@gmail.com>
2023-06-28 16:18:34 +0700

    ok

--- src/features/Event/component/Button.jsx
+++ src/features/Event/component/Button.jsx
@@ -12,10 +12,7 @@ import Maps from "./Maps";
 const initialState = {
     title: "",
     description: "",
-<<<<<<< HEAD
     placeProvince: "",
-=======
->>>>>>> googleMapAPI
     dateStart: "",
     dateEnd: "",
     capacity: "",
@@ -85,7 +82,7 @@ export default function Button() {
                 Create Event
             </label>
 
-            <input type="checkbox" id="my_modal_7" className="modal-toggle " />
+            <input type="checkbox" id="my_modal_7" className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box w-full">
                     {loading ? (
@@ -93,8 +90,7 @@ export default function Button() {
                             <span className="loading loading-spinner loading-lg"></span>
                         </div>
                     ) : (
-<<<<<<< HEAD
-                        <>
+                        <div>
                             <h3 className="text-lg font-bold text-center">
                                 Create your Event
                             </h3>
@@ -114,12 +110,15 @@ export default function Button() {
                                     onChange={handleChangeInput}
                                     name="description"
                                 />
-                                <Input
-                                    title="placeProvince"
-                                    value={input.placeProvince}
-                                    onChange={handleChangeInput}
-                                    name="placeProvince"
-                                />
+                                <div>
+                                    <h3>Location</h3>
+                                </div>
+                                <div>
+                                    <AutoCompleteComponent
+                                        setSelected={setSelected}
+                                    />
+                                    {/* <Map /> */}
+                                </div>
                                 <Input
                                     title="Date start"
                                     type="datetime-local"
@@ -147,114 +146,45 @@ export default function Button() {
                                     <select
                                         name="eventCategoryId"
                                         value={input.eventCategoryId}
-=======
-                        <div className="flex w-full ">
-                            <div>
-                                <h3 className="text-lg font-bold text-center">
-                                    Create your Event
-                                </h3>
-                                <form
-                                    className="flex flex-col gap-3 items-center"
-                                    onSubmit={handleSubmit}
-                                >
-                                    <Input
-                                        title="title"
-                                        value={input.title}
                                         onChange={handleChangeInput}
-                                        name="title"
-                                    />
-                                    <Input
-                                        title="Description"
-                                        value={input.description}
-                                        onChange={handleChangeInput}
-                                        name="description"
-                                    />
-                                    <div>
-                                        <h3>Location</h3>
-                                    </div>
-                                    <div>
-                                        <AutoCompleteComponent
-                                            setSelected={setSelected}
-                                        />
-                                        {/* <Map /> */}
-                                    </div>
-                                    {/* <Input
-                                        title="Location"
-                                        value={input.location}
-                                        onChange={handleChangeInput}
-                                        name="location"
-                                    /> */}
-                                    <Input
-                                        title="Date start"
-                                        type="datetime-local"
-                                        value={input.dateStart}
->>>>>>> googleMapAPI
-                                        onChange={handleChangeInput}
-                                        name="dateStart"
-                                    />
-                                    <Input
-                                        title="Date end"
-                                        type="datetime-local"
-                                        value={input.dateEnd}
-                                        onChange={handleChangeInput}
-                                        name="dateEnd"
-                                    />
-                                    <Input
-                                        title="Capacity"
-                                        value={input.capacity}
-                                        onChange={handleChangeInput}
-                                        name="capacity"
-                                    />
-                                    <div className="flex gap-5">
-                                        <h1 className="font-semibold">
-                                            Choose your category
-                                        </h1>
-                                        <select
-                                            name="eventCategoryId"
-                                            value={input.eventCategoryId}
-                                            onChange={handleChangeInput}
-                                        >
-                                            <option value="1">Bar</option>
-                                            <option value="2">Sport</option>
-                                            <option value="3">
-                                                Restaurant
-                                            </option>
-                                            <option value="4">Cafe</option>
-                                            <option value="5">LifeStyle</option>
-                                        </select>
-                                    </div>
+                                    >
+                                        <option value="1">Bar</option>
+                                        <option value="2">Sport</option>
+                                        <option value="3">Restaurant</option>
+                                        <option value="4">Cafe</option>
+                                        <option value="5">LifeStyle</option>
+                                    </select>
+                                </div>
+                                <InputImage
+                                    name="image1"
+                                    onChange={handleChangeFile}
+                                />
+                                <InputImage
+                                    name="image2"
+                                    onChange={handleChangeFile}
+                                />
+                                <InputImage
+                                    name="image3"
+                                    onChange={handleChangeFile}
+                                />
 
-                                    <InputImage
-                                        name="image1"
-                                        onChange={handleChangeFile}
-                                    />
-                                    <InputImage
-                                        name="image2"
-                                        onChange={handleChangeFile}
-                                    />
-                                    <InputImage
-                                        name="image3"
-                                        onChange={handleChangeFile}
-                                    />
-
-                                    <button className="bg-black text-white rounded-full w-36 h-8">
-                                        Create
-                                    </button>
-                                </form>
-                            </div>
-
-                            {/* <div className="w-96 h-80"> */}
-                            <Maps selected={selected} />
-                            {/* </div> */}
-
-                            {/* <div
-                                className=" w-screen bg-cover bg-center"
-                                style={{
-                                    backgroundImage: `url(${backgroundImage01})`,
-                                }}
-                            ></div> */}
+                                <button className="bg-black text-white rounded-full w-36 h-8">
+                                    Create
+                                </button>
+                            </form>
                         </div>
                     )}
+
+                    {/* <div className="w-96 h-80"> */}
+                    <Maps selected={selected} />
+                    {/* </div> */}
+
+                    {/* <div
+                className=" w-screen bg-cover bg-center"
+                style={{
+                    backgroundImage: `url(${backgroundImage01})`,
+                }}
+            ></div> */}
                 </div>
                 <label className="modal-backdrop" htmlFor="my_modal_7">
                     Close
